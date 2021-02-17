1880a75ceefa41cb8999d8165755eaf893fae15d
izantammafz <79188309+izantammafz@users.noreply.github.com>
2021-02-17 14:59:30 +0700

    Add files via upload

---
+++ Webserver/index.php
@@ -0,0 +1,17 @@
+
+
+<?php
+$connection=mysqli_connect('172.18.0.2','root','toor','trucorp');
+$query='SELECT * FROM Users';
+$result=mysqli_query($connection,$query);
+while($db = mysqli_fetch_array($result)){
+
+
+echo "<br>";
+echo "ID 	=  ".$db['ID']."<br>";	
+echo "Nama  	= ".$db['Nama']."<br>";
+echo "Kantor 	= ".$db['Kantor']."<br>";
+
+}
+mysqli_close($connection);
+?>
---
+++ trucorp-web-2.0/script/trucorp-db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users
+(ID int(5),
+Nama varchar(100),
+Kantor varchar(100),
+PRIMARY KEY(ID)
+);
+INSERT INTO Users(ID,Nama,Kantor) VALUES(1, 'Asep', 'Pusat'),(2,'Nana','Cabang Papua');
