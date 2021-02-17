55dccb84d9e36f2d5f0ce6b359c534aca8095f70
rootibakr <izantammafz15@gmail.com>
2021-02-17 15:17:14 +0700

    update 0.1

--- Webserver/index.php
+++ Webserver/index.php
@@ -4,6 +4,7 @@
 $connection=mysqli_connect('172.18.0.2','root','toor','trucorp');
 $query='SELECT * FROM Users';
 $result=mysqli_query($connection,$query);
+$count = 0;
 while($db = mysqli_fetch_array($result)){
 
 
@@ -11,7 +12,8 @@ echo "<br>";
 echo "ID 	=  ".$db['ID']."<br>";	
 echo "Nama  	= ".$db['Nama']."<br>";
 echo "Kantor 	= ".$db['Kantor']."<br>";
-
+$count++;
 }
+echo "Jumlah Pengguna: ".$count;
 mysqli_close($connection);
 ?>
